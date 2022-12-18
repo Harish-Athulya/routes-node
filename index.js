f5fc1102e453a76cd26369a0065fc9c7a6b33bbf
Harish-Athulya <harishath88@gmail.com>
2022-12-18 10:04:30 +0530

    Integrated Code

--- index.js
+++ index.js
@@ -17,6 +17,217 @@ app.get("/", (req, res)  => {
     res.send("ASL Test Router");
 });
 
+app.post("/login/validate", function(req, res) {
+    console.log('receiving data...');
+    var eid = req.body.employeeid;
+    var epwd = req.body.password;
+    
+    var selectQuery = `SELECT * FROM users WHERE emp_id = '${eid}' and password = '${epwd}'`;
+    
+    thgflutter.query(selectQuery, (err, results, fields) => {
+        if(err) {
+            console.log(err);
+        } 
+        else {
+            var obj = {};      
+            obj['data'] = {};
+            
+            if(results[0] == null) {
+                obj['message'] = "Failure";
+                obj['data']['employeeid'] = req.body.employeeid;
+                obj['data']['dept'] = 'Invalid';
+                obj['data']['name'] = 'Invalid';
+            }
+            else {
+                obj['message'] = "Success";
+                obj['data']['employeeid'] = req.body.employeeid;
+                obj['data']['dept'] = results[0].dept;
+                obj['data']['name'] = results[0].name;
+            }
+            res.send(obj);       
+        }
+    })    
+});
+
+app.post("/login/id", (req, res) => {
+    var eid = req.body.employeeid;
+
+    console.log(eid);
+
+    var selectQuery = `SELECT * FROM users WHERE emp_id = '${eid}'`;
+    thgflutter.query(selectQuery, (err, results, fields) => {
+        var object = {};
+        if(err) {
+            console.log(err);
+        }
+        else {
+            if(results[0] == null) {
+                object['status'] = "Invalid";
+                object['name'] = "Invalid";
+                object['dept'] = "Invalid";
+            }
+            else {
+                object['status'] = "Success";
+                object['name'] = results[0].name;
+                object['dept'] = results[0].department;
+                console.log(results[0]);
+            }
+        }
+        res.send(object);
+    });
+});
+
+var totalRooms;
+var occupiedRooms;
+var vacantRooms;
+
+function setTotalRooms(value) {
+    totalRooms = value[0].total;
+    console.log(totalRooms);
+    return totalRooms;
+}
+
+function setOccupiedRooms(value) {
+    occupiedRooms = value[0].ocp;
+    console.log(occupiedRooms);
+    return occupiedRooms;
+}
+
+function getVacantRooms() {
+    vacantRooms = totalRooms - occupiedRooms;
+    console.log(vacantRooms);
+    return vacantRooms;
+}
+
+app.get("/occupancy/count", (req, res) => {
+    var totalQuery = `SELECT COUNT(*) AS total FROM master_beds mb WHERE mb.room_id IN(SELECT mr.id FROM master_rooms mr)`;
+    var ocpQuery = `SELECT COUNT(*) AS ocp from patient_schedules ps where ps.patient_id in (select DISTINCT(patient_id) from leads) and ps.status!='Cancelled' and ps.schedule_date=curdate()`;
+
+    var total;
+    var occupied;
+
+    thgmain.query(totalQuery, (err, results, fields) => {
+        if(err) {
+            console.log(err);   
+            var data = {};
+            data['ack'] = 'failure';
+            res.send(data);
+        }
+        else {
+            totalRooms = results[0].total;
+            total = setTotalRooms(results);
+        }
+    });
+    
+    
+    thgmain.query(ocpQuery, (err, results, fields) => {
+        if(err) {
+            console.log(err);   
+            var data = {};
+            data['ack'] = 'failure';
+            res.send(data);
+        }
+        else {
+            occupied = setOccupiedRooms(results);
+            var data = {};
+            data['total'] = total;
+            data['occupied'] = occupied;
+            data['vacant'] = getVacantRooms();
+
+            res.send(data);
+        }
+
+    });
+
+});
+
+var branchTotal;
+var branchOcp;
+var branchVacant;
+
+function setBranchCount(value) {
+    branchTotal = value;
+    return branchTotal;
+}
+
+function setOccupiedCount(value) {
+    branchOcp = value;
+    return branchOcp;
+}
+
+app.get("/occupancy/count/:id", (req, res) => {
+    var branch = req.params.id;
+    var branch_id;
+    
+    switch (branch) {
+        case "Perungudi":
+            branch_id = 1;
+            break;
+        case "Arumbakkam":
+            branch_id = 2;
+            break;
+        case "Neelankarai":
+            branch_id = 3;
+            break;
+        case "Pallavaram":
+            branch_id = 4;
+            break;
+        case "Kasavanahalli":
+            branch_id = 5;
+            break;
+        default:
+            branch_id = 999;
+            break;
+    }
+
+    if(branch_id == 999) {
+        // throw "Invalid Branch Name";
+        var data = {};
+        data['ack'] = 'Failure';
+        data['error'] = 'Invalid Branch Name';
+        res.send(data);
+    } 
+
+    var branchCountQuery = `SELECT COUNT(*) AS branch_count FROM master_rooms WHERE branch_id = ${branch_id}`;
+    var branchOccupiedQuery = `SELECT COUNT(*) AS ocp_count from patient_schedules ps where ps.patient_id in (select DISTINCT(patient_id) from leads WHERE branch_id = ${branch_id}) and ps.status!='Cancelled' and ps.schedule_date=curdate()`;
+
+    
+    thgmain.query(branchCountQuery, (err, results, fields) => {
+        if(err) {
+            console.log(err);
+            var data = {};
+            data['ack'] = 'Failure';
+            data['error'] = 'SQL Error';
+            res.send(data);
+        } 
+        else {
+            branchTotal = setBranchCount(results[0].branch_count);
+            console.log(branchTotal);            
+        }        
+    });
+    
+    thgmain.query(branchOccupiedQuery, (err, results, fields) => {
+        if(err) {
+            console.log(err);
+            var data = {};
+            data['ack'] = 'Failure';
+            data['error'] = 'SQL Error';
+        }
+        else {
+            branchOcp = setOccupiedCount(results[0].ocp_count);
+            console.log(branchOcp);
+    
+            var data = {};
+            data["branch"] = branch;
+            data["total"] = branchTotal;
+            data["occupied"] = branchOcp;
+            data["vacant"] = branchTotal - branchOcp;
+            res.send(data);
+        } 
+    });
+})
+
+
 app.get("/expense/client/service", (req, res) => {
 
     var procedure_service = "SELECT l.branch_id, SUM(procedure_service.procedure_service_rate) SERVICE_COST FROM (SELECT * FROM `patient_activity_procedure_service` WHERE deleted_at IS NULL) procedure_service JOIN ( SELECT * FROM leads WHERE status = 'Ongoing') l ON procedure_service.patient_id = l.patient_id JOIN patients pp ON pp.id = procedure_service.patient_id WHERE procedure_service.schedule_date BETWEEN '2022-10-25' AND '2022-11-24' GROUP BY 1";
